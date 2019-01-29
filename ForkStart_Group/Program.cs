cebabb7b972631a77a58ba3646f913d8a49a7fc6
aashishkakar@outlook.com <aashishkakar@outlook.com>
2019-01-28 21:05:09 -0500

    updateC

--- ForkStart_Group/Program.cs
+++ ForkStart_Group/Program.cs
@@ -19,12 +19,27 @@ namespace ForkStart_Group
     {
       Debug.WriteLine("Hello World from the instructor!");
 
-      // team member A's content goes here
+            // team member A's content goes here
+<<<<<<< HEAD
 
+            // team member B's content goes here
+
+            // team member C's content goes here
+            Console.WriteLine("Hello! This is Aashish.");
+=======
+<<<<<<< HEAD
+=======
+            Console.WriteLine("Hello World");
       // team member B's content goes here
 
       // team member C's content goes here
+>>>>>>> b90d70a5bc6afaac0a2ff590f5b2c6a778f73098
 
+            // team member B's content goes here
+            Console.WriteLine("Hello World from Karthik");
+            // team member C's content goes here
+            
+>>>>>>> 29dc9111d1aa1e7eed131895cbe0d03f81f3ef91
     }
   }
 }
